92f97752607bcd824a0d9afe6317b9e5900d2ae9
cludden <chris.ludden@gmail.com>
2016-08-07 21:34:40 -0600

    adds postversion hook

--- test/tests/send-response.test.js
+++ test/tests/send-response.test.js
@@ -35,7 +35,7 @@ test('#sendResponse should wrap a callback function', function(t) {
         eventEmitter: EventEmitter
     });
     res.on('end', function() {
-        t.equal(res.statusCode, 403, 'should have status: 403');
+        t.equal(res.statusCode, 500, 'should have status: 403');
         t.error(td.verify(callback(), {times: 0}), 'should not call callback');
         t.end();
     });
@@ -43,4 +43,30 @@ test('#sendResponse should wrap a callback function', function(t) {
     const fn = td.function();
     td.when(fn()).thenCallback(true);
     fn(errorService.sendResponse(res, callback));
+});
+
+test('#sendResponse should use custom response handler if defined', function(t) {
+    const res = httpMocks.createResponse({
+        eventEmitter: EventEmitter
+    });
+    res.on('end', function() {
+        t.equal(res.statusCode, 599, 'should have status: 403');
+        t.error(td.verify(callback(), {times: 0}), 'should not call callback');
+        t.end();
+    });
+    const callback = td.function();
+    const fn = td.function();
+    td.when(fn()).thenCallback(boom.forbidden());
+    errorService.defineResponseHandler(function(res, err) {res.status(599).send(err);});
+    fn(errorService.sendResponse(res, callback));
+});
+
+test('#sendResponse should strip error from callback if successful', function(t) {
+    const res = httpMocks.createResponse();
+    const fn = td.function();
+    td.when(fn()).thenCallback(null, 'foo');
+    fn(errorService.sendResponse(res, function(data) {
+        t.equal(data, 'foo', 'should strip error');
+        t.end();
+    }));
 });
